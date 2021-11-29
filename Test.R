7ebc58372541af908001c7ead478327afa417fb0
eml113 <celestialsucculent@gmail.com>
2021-11-29 11:00:35 -0500

    Test

---
+++ Test.R
@@ -0,0 +1,17 @@
+# Write a function that converts all files in a directory with space- or tab-delimited data (.txt) 
+# into comma-separated value files.
+
+
+
+# Write a function to compile data from all .csv files in a directory into a single .csv file. The compiled
+# data should have the original twelve columns from daily data sheets, but also country and dayofYear
+# columns. The user should be able to choose whether they want to remove rows with NA’s in any
+# columns, include NAs in the compiled data but be warned of their presence, or include NAs in the
+# compiled data without a warning
+
+
+
+# Write a function to summarize the compiled data set in terms of number of screens run, percent of
+# patients screened that were infected, male vs. female patients, and the age distribution of patients.
+
+
