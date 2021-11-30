e968ecb38c919638a10c2f4d5d01944b0f86f19c
Helen Laboe <hlaboe@nd.edu>
2021-11-30 18:37:36 -0500

    Script for analysis

---
+++ analysis.R
@@ -0,0 +1,11 @@
+## Analysis for R project
+
+source("supportingFunctions.R")
+
+# Compile all data into a .csv file
+# Process data to answer questions
+## In which country (X or Y) did the outbreak likely begin?
+# 
+## If Country Y develops a vaccine for the disease, is it likely to work for Country X?
+# 
+# Provide graphical evidence 
