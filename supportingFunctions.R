401ab26c348a881413afa4a90bc8fab6bbf33225
Helen Laboe <hlaboe@nd.edu>
2021-11-30 16:41:50 -0500

    Adding country and dayofYear, need to make not as specific to directory/how file is named

--- supportingFunctions.R
+++ supportingFunctions.R
@@ -7,37 +7,48 @@ library(readr)
 
 # Combining all data from the .csv files in each country's directory to one .csv file. Add country and dayofYear columns.
 # Offer options for how to handle the entries with NAs.
-csvCombine = function(dir,country){
+csvCombine = function(dir){
+  # dir = "~/Desktop/practicedirectory/countryX"
+  # need to make not as specific
   fileNames=list.files(path=dir, pattern="*.csv", full.names=TRUE)
   allFiles=fileNames %>% 
     lapply(read_csv) %>% 
     bind_rows
-  A=as.character(readline(prompt="Be warned about NAs (yes or no): "))
+  A=as.character(readline(prompt="Be warned about NAs (Y or N): "))
   for (i in 1:length(fileNames)){
-    if (A=="yes"){
+    # i (example) = "/Users/helenlaboe/Desktop/practicedirectory/countryX/screen_175.csv"
+    # need to make not as specific
+    if (A=="Y"){
       addFile=read_csv(fileNames[i])
-      #addFile$dayofYear still trying to figure out this part
+      addFile$country=substr(dir,start=36,stop=36)
+      addFile$dayofYear=substr(fileNames[i],start=61,stop=63)
+      allFiles=addFile
       if(TRUE %in% is.na(addFile)){
         print(paste0((fileNames[i])," contains NA"))
         filesList=list()
         filesList=list(append(filesList,fileNames[i]))
-        B=as.character(readline(prompt="Remove rows with NAs (yes or no): "))
-        if (B=="yes"){
+        B=as.character(readline(prompt="Remove rows with NAs (Y or N): "))
+        if (B=="Y"){
           addFile=na.omit(addFile)
         }else{
           addFile=read_csv(fileNames[i])
-          #addFile$dayofYear 
-          allFiles=rbind(allFiles,addFile)
+          addFile$country=substr(dir,start=36,stop=36)
+          addFile$dayofYear=substr(fileNames[i],start=61,stop=63)
+          allFiles=addFile
         }
       }else{
         addFile=read_csv(fileNames[i])
-        #addFile$day
-        allFiles=rbind(allFiles,addFile)
+        addFile$country=substr(dir,start=36,stop=36)
+        addFile$dayofYear=substr(fileNames[i],start=61,stop=63)
+        allFiles=addFile
       }
     }else{
+      addFile=read_csv(fileNames[i])
+      addFile$country=substr(dir,start=36,stop=36)
+      addFile$dayofYear=substr(fileNames[i],start=61,stop=63)
+      allFiles=addFile
       print(paste0("These days contained NAs and were removed",filesList))
     }
   }
-  # allFiles$country still trying to figure out this part
   write.csv(allFiles,file="csvCombine",row.names=FALSE)
 }
