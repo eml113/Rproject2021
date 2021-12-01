dbee0980c062b92c9d806d30ad91b944f38e1de5
Helen Laboe <hlaboe@nd.edu>
2021-12-01 10:49:53 -0500

    Notes on functions and analysis

--- analysis.R
+++ analysis.R
@@ -3,9 +3,14 @@
 source("supportingFunctions.R")
 
 # Compile all data into a .csv file
-# Process data to answer questions
+countryXdf=read.csv("~/Desktop/practicedirectory/countryX/csvCombine",header=TRUE)
+countryYdf=read.csv("~/Desktop/practicedirectory/countryY/csvCombine",header=TRUE)
+merge(countryXdf,countryYdf)
+
+# Process data to answer questions and provide graphical evidence
 ## In which country (X or Y) did the outbreak likely begin?
-# 
+# Count number of each marker in each country
+# Plot each infection over time in each country (compare on a plot)
+# Number of cases occurring when and where
 ## If Country Y develops a vaccine for the disease, is it likely to work for Country X?
-# 
-# Provide graphical evidence 
+# Focused on the end of data (types and strains of bacteria in countries now)
--- supportingFunctions.R
+++ supportingFunctions.R
@@ -8,21 +8,23 @@ library(readr)
 # Combining all data from the .csv files in each country's directory to one .csv file. Add country and dayofYear columns.
 # Offer options for how to handle the entries with NAs.
 csvCombine = function(dir){
-  # dir = "~/Desktop/practicedirectory/countryX"
+  # dir = "~/Desktop/Rproject2021/countryX"
+  # NOTE: directory must be the country's directory
   # need to make not as specific
   fileNames=list.files(path=dir, pattern="*.csv", full.names=TRUE)
   allFiles=fileNames %>% 
     lapply(read_csv) %>% 
     bind_rows
+  allFiles=data.frame(matrix(ncol = 12, nrow = 0))
   A=as.character(readline(prompt="Be warned about NAs (Y or N): "))
   for (i in 1:length(fileNames)){
-    # i (example) = "/Users/helenlaboe/Desktop/practicedirectory/countryX/screen_175.csv"
+    # i (example) = "/Users/helenlaboe/Desktop/Rproject2021/countryX/screen_175.csv"
     # need to make not as specific
     if (A=="Y"){
       addFile=read_csv(fileNames[i])
-      addFile$country=substr(dir,start=36,stop=36)
-      addFile$dayofYear=substr(fileNames[i],start=61,stop=63)
-      allFiles=addFile
+      addFile$country=substr(dir,start=31,stop=31)
+      addFile$dayofYear=substr(fileNames[i],start=56,stop=58)
+      allFiles=rbind(allFiles,addFile)
       if(TRUE %in% is.na(addFile)){
         print(paste0((fileNames[i])," contains NA"))
         filesList=list()
@@ -34,19 +36,19 @@ csvCombine = function(dir){
           addFile=read_csv(fileNames[i])
           addFile$country=substr(dir,start=36,stop=36)
           addFile$dayofYear=substr(fileNames[i],start=61,stop=63)
-          allFiles=addFile
+          allFiles=rbind(allFiles,addFile)
         }
       }else{
         addFile=read_csv(fileNames[i])
         addFile$country=substr(dir,start=36,stop=36)
         addFile$dayofYear=substr(fileNames[i],start=61,stop=63)
-        allFiles=addFile
+        allFiles=rbind(allFiles,addFile)
       }
     }else{
       addFile=read_csv(fileNames[i])
       addFile$country=substr(dir,start=36,stop=36)
       addFile$dayofYear=substr(fileNames[i],start=61,stop=63)
-      allFiles=addFile
+      allFiles=rbind(allFiles,addFile)
       print(paste0("These days contained NAs and were removed",filesList))
     }
   }
