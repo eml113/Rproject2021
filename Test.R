1e7417cb96916676933c85cde2af2b3a15580c86
eml113 <celestialsucculent@gmail.com>
2021-12-02 20:18:05 -0500

    Completed third support function

--- Test.R
+++ Test.R
@@ -26,4 +26,33 @@ commaConvert <- function(directory) {
 # Write a function to summarize the compiled data set in terms of number of screens run, percent of
 # patients screened that were infected, male vs. female patients, and the age distribution of patients.
 
-
+summarizeData <- function(file) {
+  input <- read.csv(file, header = TRUE)
+  #Number of screens run
+    cat(paste0("Number of screens run: ", nrow(input), "\n"))
+  #Percent of patients infected
+    data = input[-c(2,14)]
+    sumPatients <- 0
+    for (row in 1:nrow(data)) {
+      patient <- grepl(pattern = "1", x = row)
+      if (patient==TRUE) {
+        sumPatients=sumPatients+1
+      } 
+    }
+    percentage = (sumPatients/(nrow(data)))*100
+    cat(paste0("Percentage of patients screened that were infected: ", percentage, " %\n"))
+  #Female vs. Male patients
+    sumFemale <- 0
+    sumMale <- 0
+    for (i in 1:nrow(input)) {
+      if (input$gender[i]=="female") {
+        sumFemale=sumFemale+1
+      } else if (input$gender[i]=="male") {
+        sumMale=sumMale+1
+      }
+    }
+    cat(paste0(sumFemale, " females were tested and ", sumMale, " males were tested.\n"))
+  #Age Distribution
+    cat(paste0("Statistics concerning age are as follows:\n"))
+    print(summary(input$age))
+  }
